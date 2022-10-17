f325d47b06bbccd069ec836e86668fcb2790f6be
JuniorWells <kostas.chaniotakis@gmail.com>
2022-10-18 01:50:00 +0200

    Add Controllers, GET/POST

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
@@ -9,13 +12,28 @@ namespace PlatformService.Data
 		{
 			using ( var serviceScope = app.ApplicationServices.CreateScope())
 			{
-
+				SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
 			}
 		}
 
 		private static void SeedData(AppDbContext context)
 		{
-			
+			if (!context.Platforms.Any())
+			{
+				Console.WriteLine("--> Seeding data...");
+
+				context.Platforms.AddRange(
+					new Platform() {Name="Dot Net", Publisher="Microsoft", Cost="Free"},
+					new Platform() {Name="SQL server express", Publisher="Microsoft", Cost="Free"},
+					new Platform() {Name="Kubernetes", Publisher="Cloud Native Computing Foundation", Cost="Free"}
+				);
+
+				context.SaveChanges();
+			}
+			else
+			{
+				Console.WriteLine("--> We already have data");
+			}
 		}
 	}
 }
---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+	public class PlatformsProfile : Profile
+	{
+		public PlatformsProfile()
+		{
+			// Source --> Target ( model --> dto)
+			CreateMap<Platform, PlatformReadDto>();
+			CreateMap<PlatformCreateDto, Platform>();
+		}
+	}
+}
